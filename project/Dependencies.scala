62c33888c005d6006149296f1520c3750ceab4f0
Milisav Kovacevic <mkovacevic@vast.com>
2021-08-02 23:47:03 +0200

    add dockerized services through sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,22 +1,22 @@
-import Dependencies.{akka, akkaHttp}
-import sbt._
-
-object Dependencies extends Build {
-    def akka(artifact: String): ModuleID = "com.typesafe.akka" %% artifact % "2.5.30"
-    def akkaHttp(artifact: String): ModuleID = "com.typesafe.akka" %% artifact % "10.0.5"
-
-    lazy val akkaHttpDependencies = Seq(
-        akkaHttp("akka-http-core"),
-        akkaHttp("akka-http")
-    )
-
-    lazy val akkaDependencies = Seq(
-        akka("akka-actor"),
-        akka("akka-stream")
-    ) ++ akkaHttpDependencies
-
-
-    lazy val clientDependencies = akkaHttpDependencies ++ akkaDependencies
-}
-
+//import Dependencies.{akka, akkaHttp}
+//import sbt._
+//
+//object Dependencies extends Build {
+//    def akka(artifact: String): ModuleID = "com.typesafe.akka" %% artifact % "2.5.30"
+//    def akkaHttp(artifact: String): ModuleID = "com.typesafe.akka" %% artifact % "10.0.5"
+//
+//    lazy val akkaHttpDependencies = Seq(
+//        akkaHttp("akka-http-core"),
+//        akkaHttp("akka-http")
+//    )
+//
+//    lazy val akkaDependencies = Seq(
+//        akka("akka-actor"),
+//        akka("akka-stream")
+//    ) ++ akkaHttpDependencies
+//
+//
+//    lazy val clientDependencies = akkaHttpDependencies ++ akkaDependencies
+//}
+//
 
