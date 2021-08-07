5278dbc348403c180dd9638139cc9acdf0050fd7
Milisav Kovacevic <mkovacevic@vast.com>
2021-08-07 21:49:41 +0200

    add spark container

--- spark-streaming/src/main/scala/server/CustomReceiver.scala
+++ spark-streaming/src/main/scala/server/CustomReceiver.scala
@@ -1,52 +1,52 @@
-package streaming
-
-import java.io.{BufferedReader, InputStreamReader}
-import java.net.Socket
-import java.nio.charset.StandardCharsets
-
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.receiver.Receiver
-
-class CustomReceiver(host: String, port: Int) extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) {
-
-  def onStart(): Unit = {
-    // Start the thread that receives data over a connection
-    new Thread("Socket Receiver") {
-      override def run(): Unit = {
-        receive()
-      }
-    }.start()
-  }
-
-  def onStop(): Unit = {
-    // There is nothing much to do as the thread calling receive()
-    // is designed to stop by itself isStopped() returns false
-  }
-
-  /** Create a socket connection and receive data until receiver is stopped */
-  private def receive(): Unit = {
-    var socket: Socket = null
-    var userInput: String = null
-    try {
-      println(s"Connecting to $host : $port")
-      socket = new Socket(host, port)
-      println(s"Connected to $host : $port")
-      val reader = new BufferedReader(
-        new InputStreamReader(socket.getInputStream, StandardCharsets.UTF_8))
-      userInput = reader.readLine()
-      while (!isStopped && userInput != null) {
-        store(userInput)
-        userInput = reader.readLine()
-      }
-      reader.close()
-      socket.close()
-      println("Stopped receiving")
-      restart("Trying to connect again")
-    } catch {
-      case e: java.net.ConnectException =>
-        restart(s"Error connecting to $host : $port", e)
-      case t: Throwable =>
-        restart("Error receiving data", t)
-    }
-  }
-}
+//package streaming
+//
+//import java.io.{BufferedReader, InputStreamReader}
+//import java.net.Socket
+//import java.nio.charset.StandardCharsets
+//
+//import org.apache.spark.storage.StorageLevel
+//import org.apache.spark.streaming.receiver.Receiver
+//
+//class CustomReceiver(host: String, port: Int) extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) {
+//
+//  def onStart(): Unit = {
+//    // Start the thread that receives data over a connection
+//    new Thread("Socket Receiver") {
+//      override def run(): Unit = {
+//        receive()
+//      }
+//    }.start()
+//  }
+//
+//  def onStop(): Unit = {
+//    // There is nothing much to do as the thread calling receive()
+//    // is designed to stop by itself isStopped() returns false
+//  }
+//
+//  /** Create a socket connection and receive data until receiver is stopped */
+//  private def receive(): Unit = {
+//    var socket: Socket = null
+//    var userInput: String = null
+//    try {
+//      println(s"Connecting to $host : $port")
+//      socket = new Socket(host, port)
+//      println(s"Connected to $host : $port")
+//      val reader = new BufferedReader(
+//        new InputStreamReader(socket.getInputStream, StandardCharsets.UTF_8))
+//      userInput = reader.readLine()
+//      while (!isStopped && userInput != null) {
+//        store(userInput)
+//        userInput = reader.readLine()
+//      }
+//      reader.close()
+//      socket.close()
+//      println("Stopped receiving")
+//      restart("Trying to connect again")
+//    } catch {
+//      case e: java.net.ConnectException =>
+//        restart(s"Error connecting to $host : $port", e)
+//      case t: Throwable =>
+//        restart("Error receiving data", t)
+//    }
+//  }
+//}
