a65940572cd41f025e3a2c66c55b3928ce46bdcf
Milisav Kovacevic <mkovacevic@vast.com>
2021-11-07 01:45:24 +0100

    some ecs docs

--- visualization/src/main/python/dashboards/scripts/ddl.sql
+++ visualization/src/main/python/dashboards/scripts/ddl.sql
@@ -1,9 +1,10 @@
-USE test_db;
+USE myDb;
 CREATE TABLE IF NOT EXISTS tweets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     t_key VARCHAR(255) NOT NULL,
     text VARCHAR(255) NOT NULL,
     processed_text VARCHAR(255) NOT NULL,
     polarity DOUBLE,
+    search_term VARCHAR(255),
     created VARCHAR(255)
 )
